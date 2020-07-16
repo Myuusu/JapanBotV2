0c021804a2cf4f09eb75cfa80b283de6e4a9a31f
Myuusu <68305482+Myuusu@users.noreply.github.com>
2020-07-16 13:13:23 +0200

    Update radio.py

--- commands/radio.py
+++ commands/radio.py
@@ -12,16 +12,15 @@ class Radio(commands.Cog):
     @commands.command(aliases=['r'])
     async def radio(self, ctx, url: str = 'http://87.98.217.63:23612/:stream/1'):
         global player
-        try:
-            player = await get(self.bot.voice_clients, guild=ctx.guild)
-            player.play(FFmpegPCMAudio(url))
-            player.source = discord.PCMVolumeTransformer(player.source)
-            player.source.volume = 0.07
-            await ctx.channel.send(f'You are now listening to Anime Radio!')
-            if player.is_playing():
-                player.stop()
-        except:
-            pass
+        channel = ctx.message.author.voice.channel
+        player = get(self.voice_clients, guild=ctx.guild)
+
+        if player.is_playing():
+            player.stop()
+        player.play(FFmpegPCMAudio(url))
+        player.source = discord.PCMVolumeTransformer(player.source)
+        player.source.volume = 0.07
+        await ctx.channel.send(f'You are now listening to Anime Radio!')
 
 
 def setup(bot):
