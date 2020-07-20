55cfe6de6d4dbf2c2245172c8cd1570ec4de0b93
Myuusu <68305482+Myuusu@users.noreply.github.com>
2020-07-20 18:07:40 +0200

    Update music.py

--- commands/music.py
+++ commands/music.py
@@ -1,16 +1,14 @@
 import asyncio
 import datetime
 import discord
-import humanize
 import itertools
 import re
 import sys
 import traceback
 import wavelink
+import humanize
 from discord.ext import commands
 from typing import Union
-
-RURL = re.compile('http:\/\/.+')
 
 
 class MusicController:
@@ -94,25 +92,8 @@ class Music(commands.Cog):
 
         return controller
 
-    async def cog_check(self, ctx):
-        """A local check which applies to all commands in this cog."""
-        if not ctx.guild:
-            raise commands.NoPrivateMessage
-        return True
-
-    async def cog_command_error(self, ctx, error):
-        """A local error handler for all errors arising from commands in this cog."""
-        if isinstance(error, commands.NoPrivateMessage):
-            try:
-                return await ctx.send('This command can not be used in Private Messages.')
-            except discord.HTTPException:
-                pass
-
-        print('Ignoring exception in command {}:'.format(ctx.command), file=sys.stderr)
-        traceback.print_exception(type(error), error, error.__traceback__, file=sys.stderr)
-
     @commands.command(name='connect')
-    async def connect_(self, ctx, *, channel: discord.VoiceChannel = None):
+    async def connect_(self, ctx, channel: discord.VoiceChannel = None):
         """Connect to a valid voice channel."""
         if not channel:
             try:
@@ -123,6 +104,7 @@ class Music(commands.Cog):
         player = self.bot.wavelink.get_player(ctx.guild.id)
         await ctx.send(f'Connecting to **`{channel.name}`**', delete_after=15)
         await player.connect(channel.id)
+        await player.set_volume(40)
 
         controller = self.get_controller(ctx)
         controller.channel = ctx.channel
@@ -130,7 +112,7 @@ class Music(commands.Cog):
     @commands.command()
     async def play(self, ctx, *, query: str):
         """Search for and add a song to the Queue."""
-        if not re.compile('http:\/\/.+').match(query):
+        if not re.compile('https?://.+').match(query):
             query = f'ytsearch:{query}'
 
         tracks = await self.bot.wavelink.get_tracks(f'{query}')
@@ -148,21 +130,14 @@ class Music(commands.Cog):
         await ctx.send(f'Added {str(track)} to the queue.', delete_after=15)
 
     @commands.command()
-    async def radio(self, ctx, *, query: str):
-        tracks = await self.bot.wavelink.get_tracks('https://musicbird.leanstream.co/JCB061-MP3')
-        if not tracks:
-            return await ctx.send('Could not find any songs with that query.')
-
+    async def play_station(self, ctx, query: str):
+        station = await self.bot.wavelink.get_tracks(f'{query}')
         player = self.bot.wavelink.get_player(ctx.guild.id)
         if not player.is_connected:
             await ctx.invoke(self.connect_)
-
-        track = tracks[0]
-
+        stream = station[0]
         controller = self.get_controller(ctx)
-        await controller.queue.put(track)
-        await ctx.send(f'Added {str(track)} to the queue.', delete_after=15)
-
+        await controller.queue.put(stream)
 
     @commands.command()
     async def pause(self, ctx):
@@ -274,24 +249,12 @@ class Music(commands.Cog):
 
     @commands.command(aliases=['s', 'st'])
     async def stations(self, ctx):
-        des = [
-            "**1.**  Anime Radio",
-            "**2.**  Jazz Sakura - Asia Dream Radio",
-            "**3.**  Japan Hits - Asia Dream Radio",
-            "**4.**  J-Pop Powerplay",
-            "**5.**  J-Club Powerplay Hip-Hop",
-            "**6.**  J-Pop Sakura 懐かしい",
-            "**7.**  J-Pop Powerplay Kawaii",
-            "**8.**  J-Rock Powerplay",
-            "**9.**  Vocaloid Radio",
-            "**10.** Japanimradio",
-            "**11.** FM 845",
-            "**12.** Bitter Sweet Music JP",
-            "**13.** Banana FM",
-            "**14.** Musashino FM",
-            "**15.** Radio Kanazawa",
-            "**Enter the number corresponding to the Radio you want to listen to:**"
-        ]
+        des = ["**1.**  Anime Radio", "**2.**  Jazz Sakura - Asia Dream Radio", "**3.**  Japan Hits - Asia Dream Radio",
+               "**4.**  J-Pop Powerplay", "**5.**  J-Club Powerplay Hip-Hop", "**6.**  J-Pop Sakura 懐かしい",
+               "**7.**  J-Pop Powerplay Kawaii", "**8.**  J-Rock Powerplay", "**9.**  Vocaloid Radio",
+               "**10.** Japanimradio", "**11.** FM 845", "**12.** Bitter Sweet Music JP", "**13.** Banana FM",
+               "**14.** Musashino FM", "**15.** Radio Kanazawa", "**16.** Smooth Jazz Florida",
+               "**Enter the number corresponding to the Radio you want to listen to:**"]
         embedable = discord.Embed(
             title="Station List",
             description="\n".join(des),
@@ -302,72 +265,85 @@ class Music(commands.Cog):
             return ctx.channel == m.channel and ctx.author == m.author
 
         try:
-            response = await self.bot.wait_for('message', check=pred, timeout=10)
-            tracks = ''
+            response = await self.bot.wait_for(
+                'message',
+                check=pred,
+                timeout=10
+            )
             if response.content == "1":
-                tracks = await self.bot.wavelink.get_tracks('http://87.98.217.63:23612/:stream/1')
-                await ctx.send('You are now listening to Anime Radio!')
+                station_url = 'http://87.98.217.63:23612/:stream/1'
+                name = 'Anime Radio'
             elif response.content == "2":
-                tracks = await self.bot.wavelink.get_tracks('http://agnes.torontocast.com:8087/;.mp3')
-                await ctx.send('You are now listening to Jazz Sakura - Asia DREAM Radio!')
+                station_url = 'http://agnes.torontocast.com:8087/;.mp3'
+                name = 'Jazz Sakura - Asia DREAM Radio'
             elif response.content == "3":
-                tracks = await self.bot.wavelink.get_tracks('http://agnes.torontocast.com:8102/;')
-                await ctx.send('You are now listening to Japan Hits - Asia DREAM Radio!')
+                station_url = 'http://agnes.torontocast.com:8102/;'
+                name = 'Japan Hits - Asia DREAM Radio'
             elif response.content == "4":
-                tracks = await self.bot.wavelink.get_tracks('http://bluford.torontocast.com:8526/;')
-                await ctx.send('You are now listening to J-Pop Powerplay!')
+                station_url = 'http://bluford.torontocast.com:8526/;'
+                name = 'J-Pop Powerplay'
             elif response.content == "5":
-                tracks = await self.bot.wavelink.get_tracks('http://agnes.torontocast.com:8051/;')
-                await ctx.send('You are now listening to J-Club Powerplay Hip-Hop!')
+                station_url = 'http://agnes.torontocast.com:8051/;'
+                name = 'J-Club Powerplay Hip-Hop'
             elif response.content == "6":
-                tracks = await self.bot.wavelink.get_tracks('http://bluford.torontocast.com:8519/;')
-                await ctx.send('You are now listening to J-Pop Sakura 懐かしい!')
+                station_url = 'http://bluford.torontocast.com:8519/;'
+                name = 'J-Pop Sakura 懐かしい'
             elif response.content == "7":
-                tracks = await self.bot.wavelink.get_tracks('http://sky1.torontocast.com:9029/;')
-                await ctx.send('You are now listening to J-Pop Powerplay Kawaii!')
+                station_url = 'http://sky1.torontocast.com:9029/;'
+                name = 'J-Pop Powerplay Kawaii'
             elif response.content == "8":
-                tracks = await self.bot.wavelink.get_tracks('http://cristina.torontocast.com:8057/;.mp3')
-                await ctx.send('You are now listening to J-Rock Powerplay!')
+                station_url = 'http://cristina.torontocast.com:8057/;.mp3'
+                name = 'J-Rock Powerplay'
             elif response.content == "9":
-                tracks = await self.bot.wavelink.get_tracks('http://curiosity.shoutca.st:8019/stream')
-                await ctx.send('You are now listening to Vocaloid Radio!')
+                station_url = 'http://curiosity.shoutca.st:8019/stream'
+                name = 'Vocaloid Radio'
             elif response.content == "10":
-                tracks = await self.bot.wavelink.get_tracks('http://vps-8ef514a5.vps.ovh.net:8000/stream')
-                await ctx.send('You are now listening to Japanimradio!')
+                station_url = 'http://vps-8ef514a5.vps.ovh.net:8000/stream'
+                name = 'Japanimradio'
             elif response.content == "11":
-                tracks = await self.bot.wavelink.get_tracks('https://musicbird.leanstream.co/JCB104-MP3')
-                await ctx.send('You are now listening to FM 845!')
+                station_url = 'https://musicbird.leanstream.co/JCB104-MP3'
+                name = 'FM 845'
             elif response.content == "12":
-                tracks = await self.bot.wavelink.get_tracks('http://5.196.244.141:8600/live.jp')
-                await ctx.send('You are now listening to Bitter Sweet Music JP!')
+                station_url = 'http://5.196.244.141:8600/live.jp'
+                name = 'Bitter Sweet Music JP'
             elif response.content == "13":
-                tracks = await self.bot.wavelink.get_tracks('https://musicbird.leanstream.co/JCB075-MP3')
-                await ctx.send('You are now listening to Banana FM!')
+                station_url = 'https://musicbird.leanstream.co/JCB075-MP3'
+                name = 'Banana FM'
             elif response.content == "14":
-                tracks = await self.bot.wavelink.get_tracks('https://musicbird.leanstream.co/JCB032-MP3')
-                await ctx.send('You are now listening to Musashino FM!')
+                station_url = 'https://musicbird.leanstream.co/JCB032-MP3'
+                name = 'Musashino FM'
             elif response.content == "15":
-                tracks = await self.bot.wavelink.get_tracks('https://musicbird.leanstream.co/JCB061-MP3')
-                await ctx.send('You are now listening to Radio Kanazawa!')
-
+                station_url = 'https://musicbird.leanstream.co/JCB061-MP3'
+                name = 'Radio Kanazawa'
+            elif response.content == "16":
+                station_url = 'https://usa6.fastcast4u.com/proxy/wsjfhd?mp=/1'
+                name = 'Smooth Jazz Florida'
             else:
-                await ctx.send('Bad Input')
-            if not tracks:
                 return await ctx.send('Could not find any songs with that query.')
             player = self.bot.wavelink.get_player(ctx.guild.id)
-            if not player.is_connected:
-                try:
-                    channel = ctx.author.voice.channel
-                except AttributeError:
-                    raise discord.DiscordException(
-                        'No channel to join. Please either specify a valid channel or join one.')
-
-                await ctx.send(f'Connecting to **`{channel.name}`**', delete_after=15)
-                await player.connect(channel.id)
-                await player.play(tracks[0])
-
+            if player.is_connected:
+                await player.stop()
+            await self.play_station(ctx, station_url)
+            await ctx.send(f'You are now listening to: {name}!')
         except asyncio.TimeoutError:
             await ctx.send('Too Slow')
+
+    async def cog_check(self, ctx):
+        """A local check which applies to all commands in this cog."""
+        if not ctx.guild:
+            raise commands.NoPrivateMessage
+        return True
+
+    async def cog_command_error(self, ctx, error):
+        """A local error handler for all errors arising from commands in this cog."""
+        if isinstance(error, commands.NoPrivateMessage):
+            try:
+                return await ctx.send('This command can not be used in Private Messages.')
+            except discord.HTTPException:
+                pass
+
+        print('Ignoring exception in command {}:'.format(ctx.command), file=sys.stderr)
+        traceback.print_exception(type(error), error, error.__traceback__, file=sys.stderr)
 
 
 def setup(bot):
