5ca93567aca8946c142a280ae4af769efc35bb60
BrandonEnglish848 <btoplafayette@gmail.com>
2020-07-29 08:09:09 -0500

    Added Static Variables to Storage

---
+++ storage/station_list.py
@@ -0,0 +1,75 @@
+station_list = [
+    {
+        'id': 1,
+        'name': 'Anime Radio',
+        'url': 'http://87.98.217.63:23612/:stream/1'
+    }, {
+        'id': 2,
+        'name': 'Jazz Sakura - Asia Dream Radio',
+        'url': 'http://agnes.torontocast.com:8087/;.mp3'
+    }, {
+        'id': 3,
+        'name': 'Japan Hits - Asia Dream Radio',
+        'url': 'http://agnes.torontocast.com:8102/;'
+    }, {
+        'id': 4,
+        'name': 'J-Pop Powerplay',
+        'url': 'http://bluford.torontocast.com:8526/;'
+    }, {
+        'id': 5,
+        'name': 'J-Club Powerplay Hip-Hop',
+        'url': 'http://agnes.torontocast.com:8051/;'
+    }, {
+        'id': 6,
+        'name': 'J-Pop Sakura 懐かしい',
+        'url': 'http://bluford.torontocast.com:8519/;'
+    }, {
+        'id': 7,
+        'name': 'J-Pop Powerplay Kawaii',
+        'url': 'http://sky1.torontocast.com:9029/;'
+    }, {
+        'id': 8,
+        'name': 'J-Rock Powerplay',
+        'url': 'http://cristina.torontocast.com:8057/;.mp3'
+    }, {
+        'id': 9,
+        'name': 'Vocaloid Radio',
+        'url': 'http://curiosity.shoutca.st:8019/stream'
+    }, {
+        'id': 10,
+        'name': 'Japanimradio',
+        'url': 'http://vps-8ef514a5.vps.ovh.net:8000/stream'
+    }, {
+        'id': 11,
+        'name': 'FM 845',
+        'url': 'https://musicbird.leanstream.co/JCB104-MP3'
+    }, {
+        'id': 12,
+        'name': 'Bitter Sweet Music JP',
+        'url': 'http://5.196.244.141:8600/live.jp'
+    }, {
+        'id': 13,
+        'name': 'Banana FM',
+        'url': 'https://musicbird.leanstream.co/JCB075-MP3'
+    }, {
+        'id': 14,
+        'name': 'Musashino FM',
+        'url': 'https://musicbird.leanstream.co/JCB032-MP3'
+    }, {
+        'id': 15,
+        'name': 'Radio Kanazawa',
+        'url': 'https://musicbird.leanstream.co/JCB061-MP3'
+    }, {
+        'id': 16,
+        'name': 'Smooth Jazz Florida',
+        'url': 'https://usa6.fastcast4u.com/proxy/wsjfhd?mp=/1'
+    }, {
+        'id': 17,
+        'name': '97.9 Home Radio',
+        'url': 'http://149.56.147.197:9071/stream'
+    }, {
+        'id': 18,
+        'name': 'Starred on Spotify',
+        'url': 'spotify:playlist:5Fi71Kq5x6kDkf3mHEDDSf'
+    }
+]
