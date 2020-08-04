13fec1db434dc3eb75bb4cc2d74d3c76a3c17f33
BrandonEnglish848 <btoplafayette@gmail.com>
2020-08-04 06:31:02 -0500

    Updated Config Template

--- config.py
+++ config.py
@@ -1,9 +1,11 @@
 bot_token = ""
-node_settings = {
-    'host': "127.0.0.1",
-    'port': 80,
-    'rest_uri': "http://127.0.0.1:80",
-    'password': "",
-    'identifier': "",
-    'region': "europe"
-}
+chrome_driver_path = "C:\\JapanBotV2\\Lavalink\\chromedriver.exe"
+bot_prefix = ["!", "'", "73"]
+node_settings = dict(
+    host="127.0.0.1",
+    port=80,
+    rest_uri="http://127.0.0.1:80",
+    password="",
+    identifier="",
+    region="europe"
+)
