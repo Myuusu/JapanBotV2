8698e3f100ea3459647573408fe8b37d2fc9cc0d
BrandonEnglish848 <btoplafayette@gmail.com>
2020-08-20 07:22:15 -0500

    Added placeholder for Mongo_db_url

--- config.py
+++ config.py
@@ -1,10 +1,11 @@
-bot_token = ""
-chrome_driver_path = ""
+bot_token = "<token>"
+chrome_driver_path = "C:\\Users\\<User>\\PycharmProjects\\JapanBotV2\\Lavalink\\chromedriver.exe"
 node_settings = dict(
-    host="",
-    port="",
-    rest_uri="",
-    password="",
-    identifier="",
-    region=""
+    host="127.0.0.1",
+    port=80,
+    rest_uri="http://127.0.0.1:80",
+    password="<Password>",
+    identifier="<UserName",
+    region="europe"
 )
+mongo_db_url = "mongodb+srv://<UserName>:<Password>@japanbot.abc8v.mongodb.net/test"
